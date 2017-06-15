bce08276c86df37ed33ec385f75db3cc20779810
kashish <kashish@protonmail.com>
2017-06-15 18:15:11 +0530

    Third commit

---
+++ mysite/xmldata/sub_dir.py
@@ -0,0 +1,5 @@
+import os,sys
+
+def subdirs(path_of_dir):
+     list_of_subdir = os.listdir(path_of_dir)
+     return(list_of_subdir)
--- mysite/xmldata/views.py
+++ mysite/xmldata/views.py
@@ -7,12 +7,17 @@ def index(request):
     return render(request, 'xmldata/form.html')
 
 def result(request):
-    if request.method == 'POST':
-        root_path = request.POST.get('textfield', "")
-        html = xml_output(root_path) 
-        context = {'html' : html }
-        return(render(request, 'xmldata/myfile.xml',context,content_type="xml/force-download"))
-       
+    try:
+        if request.method == 'POST':
+            root_path = request.POST.get('textfield', "")
+            html = xml_output(root_path)
+            context = {'html' : html}
+            return(render(request, 'xmldata/myfile.xml',context,content_type="xml/force-download"))
+    except FileNotFoundError:
+        return(HttpResponse("Entered Directory does not exist.<br><br><a href='/xmldata/index'>Try Again</a>"))
+    except PermissionError:
+        return(HttpResponse("You don't have permission to access (some) contents of this Directory.<br><br><a href='/xmldata/index'>Try Again</a"))
+
 def realview(request):
     return(HttpResponse("XML Creator main Page<br><br><a href='/xmldata/index'>Create XML</a>"))
 
